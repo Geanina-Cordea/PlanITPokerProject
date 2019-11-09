61434f88c127c3b727f8bb09f36ae39dd3d1d1c6
Geanina Cordea <geanina.cordea@yahoo.com>
2019-11-10 00:18:45 +0200

    first commit

--- PlanITPokerProject/UnitTest1.cs
+++ PlanITPokerProject/UnitTest1.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
-namespace PlanITPokerProject
+namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+        //test
         [TestMethod]
         public void TestMethod1()
         {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.planitpoker.com/authentication/restorepassword/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("inputEmail")).SendKeys("ann.mk@gmail.com");
+            driver.FindElement(By.ClassName("btn")).Click();
+            System.Threading.Thread.Sleep(4000);
+            driver.Close();
+
         }
     }
 }
