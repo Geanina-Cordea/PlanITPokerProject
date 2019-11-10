913f4777294afa89c671618f5fe3983ca597ccd1
Geanina Cordea <geanina.cordea@yahoo.com>
2019-11-10 22:23:34 +0200

    new tests added

---
+++ PlanITPokerProject/Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace LoginUnitTest1
+{
+    [TestClass]
+    public class LoginTest1
+    {
+        [TestMethod]
+        public void LoginMethod()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.planitpoker.com/authentication/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("inputEmail")).SendKeys("ann.mk@gmail.com");
+            driver.FindElement(By.Name("inputPassword")).SendKeys("C@sadelemn38");
+            driver.FindElement(By.ClassName("btn")).Click();
+            System.Threading.Thread.Sleep(4000);
+            driver.Close();
+        }
+    }
+}
