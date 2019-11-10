4701a42225d541c2b135312ec89ad4cedae04a0d
Geanina Cordea <geanina.cordea@yahoo.com>
2019-11-10 15:31:23 +0200

    New test added

--- PlanITPokerProject/Forgetten Password-No Email.cs
+++ PlanITPokerProject/Forgetten Password-No Email.cs
@@ -3,19 +3,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace ForgettenPasswordUnitTest1
+namespace ForgettenPasswordUnitTest2
 {
     [TestClass]
-    public class ForgettenPasswordTest1
+    public class ForgettenPasswordTest2
     {
-       
         [TestMethod]
-        public void ForgettenPasswordMethod()
+        public void ForgettenPasswordNoEmailMethod()
         {
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.planitpoker.com/authentication/restorepassword/");
             driver.Manage().Window.Maximize();
-            driver.FindElement(By.Name("inputEmail")).SendKeys("ann.mk@gmail.com");
+            driver.FindElement(By.Name("inputEmail")).SendKeys("");
             driver.FindElement(By.ClassName("btn")).Click();
             System.Threading.Thread.Sleep(4000);
             driver.Close();
