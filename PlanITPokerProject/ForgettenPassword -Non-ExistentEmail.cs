dc18f598730e09595e6d9de8390e31e4033566af
Geanina Cordea <geanina.cordea@yahoo.com>
2019-11-10 15:39:48 +0200

    new test added

---
+++ PlanITPokerProject/ForgettenPassword -Non-ExistentEmail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ForgettenPasswordUnitTest3
+{
+    [TestClass]
+    public class ForgettenPasswordTest3
+    {
+        [TestMethod]
+        public void ForgettenPasswordNonExistentEmailMethod()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.planitpoker.com/authentication/restorepassword/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("inputEmail")).SendKeys("ale.mike@gmail.com");
+            driver.FindElement(By.ClassName("btn")).Click();
+            System.Threading.Thread.Sleep(4000);
+            driver.Close();
+
+        }
+    }
+}
