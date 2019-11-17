59b8611013c1bdb3cd9af31c432c4f0b48026a87
Geanina Cordea <geanina.cordea@yahoo.com>
2019-11-17 22:51:45 +0200

    Quick Play - Page Object Pattern

---
+++ PlanITPokerProject/QuickPlay.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanITPokerProject
+{
+    public class QuickPlay
+    {
+        IWebDriver driver;
+        By Username = By.Name("inputName");
+        By EnterButton = By.ClassName("btn");
+
+        public QuickPlay(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void TypeUsername()
+        {
+            driver.FindElement(Username).SendKeys("Ana");
+        }       
+        public void ClickOnEnterButton()
+        {
+            driver.FindElement(EnterButton).Click();
+        }
+
+    }
+}
